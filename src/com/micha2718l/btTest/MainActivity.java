d219d28eb06649088bfc9b92602f98b3ad7de9e1
micha2718l <micha2718l@gmail.com>
2015-11-04 10:00:49 -0600

    first fully working color select and send

--- src/com/micha2718l/btTest/MainActivity.java
+++ src/com/micha2718l/btTest/MainActivity.java
@@ -11,16 +11,27 @@ import android.view.View;
 import android.widget.TextView;
 import android.widget.EditText;  
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.SeekBar;
+import android.graphics.Color;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Set;
 import java.util.UUID;
+import android.widget.SeekBar.*;
+
 
 public class MainActivity extends Activity
 {
 	TextView myLabel;
     EditText myTextbox;
+	ImageView imageColor;
+	Color myColor;
+	float hsvC[] = {180,1,1};
+	int colorArduino = 25;
+	int progFinal = 25;
+	SeekBar colorPick;
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket mmSocket;
     BluetoothDevice mmDevice;
@@ -46,9 +57,40 @@ public class MainActivity extends Activity
         Button buttonOff = (Button) findViewById(R.id.buttonOff);
         Button buttonSend = (Button) findViewById(R.id.buttonSend);
         
-        
+		colorPick = (SeekBar)findViewById(R.id.seekBar1);
+        imageColor = (ImageView)findViewById(R.id.imageView1);
         myLabel = (TextView)findViewById(R.id.textView2);
-        
+        //myColor = Color.HSVToColor(hsvC);
+		imageColor.setBackgroundColor(Color.HSVToColor(hsvC));
+		
+		
+		colorPick.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener()
+		{
+			public void onProgressChanged(SeekBar s, int prog, boolean mysteryBool)
+			{
+				colorArduino = prog/2-1;
+				if(colorArduino<0){colorArduino=0;}
+				myLabel.setText(Integer.toString(colorArduino));
+				progFinal = prog;
+				hsvC[0] = prog*3.6f;
+				imageColor.setBackgroundColor(Color.HSVToColor(hsvC));
+				
+			}
+			
+			public void onStartTrackingTouch(SeekBar s)
+			{
+
+			}
+			
+			public void onStopTrackingTouch(SeekBar s)
+			{
+				
+			}
+
+				
+				
+		});
+		
         buttonOn.setOnClickListener(new View.OnClickListener()
         {
           public void onClick(View v)
@@ -73,8 +115,12 @@ public class MainActivity extends Activity
         {
           public void onClick(View v)
           {
-        	  TextView text1 = (TextView) findViewById(R.id.textView1);
-        	  text1.setText("");
+        	  try 
+              {
+				  myLabel.setText("c"+String.format("%02d",colorArduino));
+                  sendData("c"+String.format("%02d",colorArduino));
+              }
+              catch (IOException ex) { }
         	  
           }
         });
@@ -85,7 +131,7 @@ public class MainActivity extends Activity
           {
         	  try 
               {
-                  sendData();
+                  sendData("s03");
               }
               catch (IOException ex) { }
         	  
@@ -96,9 +142,9 @@ public class MainActivity extends Activity
         
     }
     
-    void sendData() throws IOException
+    void sendData(String msg) throws IOException
     {
-        String msg = "s03";
+        //String msg = "s03";
         msg += "\n";
         mmOutputStream.write(msg.getBytes());
         myLabel.setText("Data Sent");
@@ -132,14 +178,16 @@ public class MainActivity extends Activity
         {
             for(BluetoothDevice device : pairedDevices)
             {
-                if(device.getName().equals("MattsBlueTooth")) 
+				//myLabel.setText(device.getName());
+                if(device.getName().equals("HC-06")) 
                 {
                     mmDevice = device;
+					myLabel.setText("Found: " + device.getName());
                     break;
                 }
             }
         }
-        myLabel.setText("Bluetooth Device Found");
+        //myLabel.setText("Bluetooth Device Found");
     }
     
     void openBT() throws IOException
